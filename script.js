8ac06d7dcfa1b60bede81bd957d4b4045f5cca5f
Roney-juma <ochirone10@gmail.com>
2021-04-11 02:16:47 +0300

    added some css part and modified html

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function inputs(){
     var male=["Kwasi","kwodwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-    var cc=parseInt(birth_date.substr());
-    var mm=parseInt(birth_date.substr());
-    var yy=parseInt(birth_date.substr());
-    var dd=parseInt(birth_date.substr());
+    var cc=parseInt(birth_date.substr(7,8));
+    var mm=parseInt(birth_date.substr(0,1));
+    var yy=parseInt(birth_date.substr(9,10));
+    var dd=parseInt(birth_date.substr(3,4));
     var d=parseInt((((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd)% 7)
     
     if (gender==="Male"){
