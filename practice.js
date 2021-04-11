92324a56962557603aea905ef5ba81f82b162a6e
Roney-juma <ochirone10@gmail.com>
2021-04-11 22:22:04 +0300

    added the nabar and linked the css

---
+++ practice.js
@@ -0,0 +1,3 @@
+var week=(((19/4)-2*19-1) + ((5*98/4) ) + ((26*(05+1)/10))+28)%7;
+var day=Math.floor(week);
+console.log(day)
--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 var submit=document.getElementById("submit");
-submit.onclick= function (){
-    console.log(inputs())
-};
-function inputs(){
-    var gender=document.querySelectorAll(".form-select");
+submit.onclick= main
+
+function main(){
+    var gender=document.getElementById("selec").value;
     var birth_date=document.getElementById("birth-date").value;
 
     var male=["Kwasi","kwodwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-    var cc=parseInt(birth_date.substr(7,8));
+    var cc=parseInt(birth_date.substr(4,5));
     var mm=parseInt(birth_date.substr(0,1));
-    var yy=parseInt(birth_date.substr(9,10));
-    var dd=parseInt(birth_date.substr(3,4));
-    var d=parseInt((((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd)% 7)
+    var yy=parseInt(birth_date.substr(6,7));
+    var dd=parseInt(birth_date.substr(2,3));
+    var d=parseInt((((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd)% 7);
+    var day=Math.floor(d);
     
     if (gender==="Male"){
-        alert("Your Akan name is: " + male[d])
+        alert("Your Akan name is: " + male[day]);
     }
     else if(gender==="Female"){
-        alert("Your Akan name is: " + female[d])
+        alert("Your Akan name is: " + female[day]);
     }
     else{
         alert("Please specify your gender to be given the name")
