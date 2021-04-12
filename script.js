92d99cee43e6ff0c1251fca96c944ecc6fa97838
Roney-juma <ochirone10@gmail.com>
2021-04-12 12:48:41 +0300

    working on the js to work with html

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-var submit=document.getElementById("submit");
-var gender=document.getElementById("selec").value;
-var birth_date=document.getElementById("birth-date").value;
-var male=["Kwasi","kwodwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+// var submit= document.getElementById("submit");
+var gender= document.getElementById("selec").value;
+var birthString= document.getElementById("birth-date").value;
 
 
-submit.onclick= function (){
-    console.log(main());
+
+function submit(){
+    console.log(main(birthString,gender));
 }
 
 
 
-function main(){
-    var cc=parseInt(birth_date.substr(4,5));
-    var mm=parseInt(birth_date.substr(0,1));
-    var yy=parseInt(birth_date.substr(6,7));
-    var dd=parseInt(birth_date.substr(2,3));
-    var d=parseInt((((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd)% 7);
-    var day=Math.floor(d);
+function main(birthString,gender){
+    var birth_date= new Date(birthString);
+    var day=birth_date.getDay();
+    var male=["Kwasi","kwodwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     
-    if (gender==="Male"){
+    if (gender=="Male"){
         alert("Your Akan name is: " + male[day]);
     }
-    else if(gender==="Female"){
+    else if(gender=="Female"){
         alert("Your Akan name is: " + female[day]);
     }
     else{
-        alert("Please specify your gender to get your respective Akan name!!")
+        alert("Please specify your gender to get your respective Akan name!!");
     }
 
 
